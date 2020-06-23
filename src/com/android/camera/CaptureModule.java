f0c1486bd9e6257f28df4ca9e259fefd7212a4b5
Jake Weinstein <jake@aospa.co>
2020-06-24 00:26:21 +0200

    CaptureModule: Hide Zoom seekbar and button in all cases
    Probably helpful for camera development but ugly to show
    all the time.
    
    Change-Id: I3e89213297f80c5caa827558e55a2a3375decf94

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -9110,12 +9110,7 @@ public class CaptureModule implements CameraModule, PhotoController,
     }
 
     public void updateZoomSeekBarVisible() {
-        if (mCurrentSceneMode.mode == CameraMode.PRO_MODE ||
-                mCurrentSceneMode.mode == CameraMode.RTB || mIsRTBCameraId) {
             mUI.hideZoomSeekBar();
-        } else {
-            mUI.showZoomSeekBar();
-        }
     }
 
     public void setCurrentSceneModeOnly(int mode) {
